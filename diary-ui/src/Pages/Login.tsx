86ff312bf2cdfbc3d2c630343d96b70b08d8f18e
Vladimir Kutsev <vladimirkucev6@gmail.com>
2023-11-06 19:17:43 +0200

    ui kitten added and basic test on the login page

--- diary-ui/src/Pages/Login.tsx
+++ diary-ui/src/Pages/Login.tsx
@@ -1,12 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import React, {useState} from 'react'
 
 type Props = {}
 
 const Login = (props: Props) => {
+    const [text, setText] = useState(null)
+    const handlePress = async () => {
+        try {
+            const response = await fetch('http://localhost:3001/', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            })
+            const data = await response.text()
+            console.log(response)
+            setText(data)
+        } catch (error) {
+            console.warn(error);
+            
+        }
+    }
     return (
         <View>
-            <Text>Login</Text>
+            <Text>{text}</Text>
+            <Button title="Go to Home" onPress={handlePress} />
         </View>
     )
 }
