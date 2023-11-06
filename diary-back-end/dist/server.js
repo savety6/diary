4cd79b2375c7d9f62572e696c2474211d933184a
Vladimir Kutsev <vladimirkucev6@gmail.com>
2023-11-06 16:26:48 +0200

    setup

---
+++ diary-back-end/dist/server.js
@@ -0,0 +1,3 @@
+import * as fs from 'fs';
+console.log(fs.readFileSync('./package.json', 'utf8'));
+//# sourceMappingURL=server.js.map
---
+++ diary-back-end/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const PORT = 3001;
+
+app.get('/', (_req, res) => {
+    console.log('someone pinged here');
+    res.send('pong');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http:/localhost:${PORT}`);
+});
